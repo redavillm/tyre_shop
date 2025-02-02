37da836e05c168408c55dd11616c5d2a4c84d899
redavillm <leonovvoleg@yandex.ru>
2025-02-02 22:10:01 +0300

    fixed cors params for admin panel

--- server/app.js
+++ server/app.js
@@ -37,6 +37,9 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+app.options("*", cors(corsOptions));
+
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cookieParser());
