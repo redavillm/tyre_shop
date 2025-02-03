e2b085e193cb8f15386107fa432af0fa60d53598
redavillm <leonovvoleg@yandex.ru>
2025-02-03 21:16:50 +0300

    mail settings

--- server/services/sendEmail.js
+++ server/services/sendEmail.js
@@ -5,22 +5,15 @@ const MAIL_HOST = process.env.MAIL_HOST;
 const MAIL_ADDRESS = process.env.MAIL_ADDRESS;
 const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
 
-console.log("process.env => ", process.env);
-console.log("MAIL_HOST => ", MAIL_HOST);
-console.log("MAIL_ADDRESS => ", MAIL_ADDRESS);
-console.log("MAIL_PASSWORD => ", MAIL_PASSWORD);
-
 // Настройка транспорта
 const transporter = nodemailer.createTransport({
-  host: MAIL_HOST, // Замените на ваш SMTP-сервер
+  host: MAIL_HOST, // Ваш SMTP-сервер
   port: 2525, //Порт SMTP шифрованый
   secure: false, // Используйте false для TLS, true для SSL (порт 465)
   auth: {
-    user: MAIL_ADDRESS, //Ваш email
+    user: MAIL_ADDRESS, //Ваш email отправителя
     pass: MAIL_PASSWORD, //Пароль
   },
-  logger: true, //dev
-  debug: true, //dev
 });
 
 const sendEmail = async (to, subject, text, html) => {
