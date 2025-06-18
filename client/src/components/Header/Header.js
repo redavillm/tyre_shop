b1cf3772c00a34e5f996e1d6f3f60d8eb6ce014c
redavillm <leonovvoleg@yandex.ru>
2025-06-18 23:20:48 +0300

    test commit, added new domain and direcotry

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,19 +8,24 @@ const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 21px 29px;
   background-color: var(--bg-grey);
+  padding: 10px 20px;
 `;
 
 // Стили для логотипа
 const LogoLink = styled(Link)`
   display: inline-block;
   img {
-    max-height: 50px;
+    max-height: 80px;
     @media (max-width: 768px) {
       max-height: 40px;
     }
   }
+`;
+
+const SearchBar = styled.input`
+  padding: 5px;
+  width: 300px;
 `;
 
 export const Header = () => {
@@ -29,9 +34,9 @@ export const Header = () => {
       <LogoLink to="/">
         <img src="/img/flame_tyre_logo.png" alt="Flame Tyre Logo" />
       </LogoLink>
-      <input />
-      <h5>Контакты</h5>
-      <h5>+7 (931) 341-58-57</h5>
+      <SearchBar placeholder="Поиск..." />
+      <Link href="#">Контакты</Link>
+      <Link href="tel:+79313415857">+7 (931) 341-58-57</Link>
       <CartBtn />
     </StyledHeader>
   );
